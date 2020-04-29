f0c97b63d7e572e9017361456785d363941f0d12
quanzhenjie <279692357@qq.com>
2020-04-29 10:05:50 +0800

    增加cookie支持

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './plugins/iview.js'
 import App from './App.vue'
 import router from './plugins/router.js'
 import store from './plugins/store.js'
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
 
 Vue.config.productionTip = false
 
